a111d7cc6088983d46f99cc1eccb7018c738c515
robsonabf <32685128+robsonabf@users.noreply.github.com>
2022-12-23 17:14:35 -0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,18 +1,5 @@
 from django.db import models
 
-class Login(models.Model):
-    usuario = models.EmailField(max_length=80)
-    senha = models.CharField(max_length=20)
-
-    def __str__(self):
-        pass
-
-class Cadastro(models.Model):
-    nome = models.CharField(max_length=50)
-    email = models.EmailField(max_length=80)
-    senha = models.CharField(max_length=60)
-    confSenha = models.CharField(max_length=60)
-
 
 class CadastroProduto(models.Model):
     Emb_choices = (
@@ -24,6 +11,7 @@ class CadastroProduto(models.Model):
     descricao = models.CharField(max_length=20)
     tipoEmb = models.CharField(max_length=2, choices=Emb_choices)
     codEmb = models.CharField(max_length=14)
+
 
 class CadastroEmpresa(models.Model):
     razaosocial = models.CharField(max_length=15)
