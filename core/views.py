d40c2ac20bf894f90537b16849e424134c8d2a78
robsonabf <32685128+robsonabf@users.noreply.github.com>
2022-12-23 17:15:51 -0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,28 +1,70 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
-
-from .models import Login
-
-from django.urls import reverse_lazy 
-
+from django.urls import reverse_lazy
 from django.shortcuts import render # Já tinha
-
 from .forms import Login # Já tinha
+from django.contrib.auth.models import User
+from .models import CadastroEmpresa as cadempresa
 
 
 def core(request):
-    return render(request, 'index.html')
+    if request.method == "GET":
+        return render(request, 'index.html')
+    else:
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
 
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login(request, user)
+            return HttpResponse('Autenticado!')
+        else:
+            return HttpResponse('Usuário ou senha inválido!')
+
+
+@login_required
 def TelaUsuario(request):
-    return render(request, 'tela-usuario.html')
+        return render(request, 'tela-usuario.html')
+
 
 def Cadastro(request):
-    return render(request, 'cadastro.html')
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
 
+        user = User.objects.filter(username=username).first()
+        if user:
+            return HttpResponse('Já existe um usuário cadastrado com esse nome.')
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+        return HttpResponse('Usuário cadastrado com sucesso!')
+
+
+@login_required
 def CadastroProduto(request):
     return render(request, 'cadastro-produto.html')
 
+
+@login_required
 def CadastroEmpresa(request):
-    return render(request, 'cadastro-empresa.html')
+    if request.method == "GET":
+        return render(request, 'cadastro-empresa.html')
+    else:
+        rs = request.POST.get('empresa')
+        cnpj = request.POST.get('cnpj')
+        endereco = request.POST.get('endereco')
+        cep = request.POST.get('cep')
+
+        empresa = cadempresa.objects.create(razaosocial=rs, cnpj=cnpj, endereco=endereco, cep=cep)
+        empresa.save()
+        return HttpResponse('Empresa cadastrada com sucesso!')
+
 
 # def get_name(request):
 #     if request.method == 'POST':
