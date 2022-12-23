3577b9dc42189e6b616fe23b03b76ff8967c036e
robsonabf <32685128+robsonabf@users.noreply.github.com>
2022-12-23 17:16:39 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-
 from. models import *
 
-admin.site.register(Login)
+
+admin.site.register(CadastroEmpresa)
+admin.site.register(CadastroProduto)
 
 # Register your models here.
